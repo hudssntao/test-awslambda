e9782585d9d1f2c1ace59efd7f015cfb2acdf778
hudssntao <hudsontao@gmail.com>
2024-08-08 17:11:41 -0400

    test: aws auto shift to latest image?

--- apps/lambda/src/firstLambda/index.ts
+++ apps/lambda/src/firstLambda/index.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 import env from "@repo/env";
 
 export async function handler(event: APIGatewayProxyEvent) {
-  console.log("running first lambda: ", env.TEST_ENV);
+  console.log("running first lambda v2: ", env.TEST_ENV);
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello from Lambda.", env: env.TEST_ENV }),
